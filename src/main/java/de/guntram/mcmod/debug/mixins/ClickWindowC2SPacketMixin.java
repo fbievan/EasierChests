02b7bb460bb52d61bc819e60288df5d695193ad3
Evan G <fbievan33@protonmail.com>
2026-03-08 03:07:43 +0000

    it works kinda [v2]

--- src/main/java/de/guntram/mcmod/debug/mixins/ClickWindowC2SPacketMixin.java
+++ src/main/java/de/guntram/mcmod/debug/mixins/ClickWindowC2SPacketMixin.java
@@ -1,7 +1,6 @@
 package de.guntram.mcmod.debug.mixins;
 
 import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
-import net.minecraft.screen.slot.SlotActionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,10 +13,9 @@ public class ClickWindowC2SPacketMixin {
 
     static private final Logger LOGGER = LoggerFactory.getLogger("EasierChests");
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void dumpC2SNewInfo(int syncid, int revision, int slot, int button, SlotActionType actionType,
-            net.minecraft.item.ItemStack carriedStack, java.util.Map<Integer, net.minecraft.item.ItemStack> modifiedSlots,
-            CallbackInfo ci) {
-        LOGGER.debug("new ClickSlot C2S: syncid={}, slot={}, button={}, action={}", syncid, slot, button, actionType);
+    @Inject(method = "<init>(IISBLnet/minecraft/screen/slot/SlotActionType;Lit/unimi/dsi/fastutil/ints/Int2ObjectMap;Lnet/minecraft/screen/sync/ItemStackHash;)V", at = @At("RETURN"))
+    private void dumpC2SNewInfo(CallbackInfo ci) {
+        ClickSlotC2SPacket self = (ClickSlotC2SPacket)(Object)this;
+        LOGGER.debug("new ClickSlot C2S: syncid={}, slot={}, button={}, action={}", self.syncId(), self.slot(), self.button(), self.actionType());
     }
 }
